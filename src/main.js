d65beaaaff088d13088dc8f21546a75beba4ce85
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-10 17:22:29 -0700

    added a picture tried to made scrolling function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,39 @@
 import './index.css'
 import 'flowbite'
+
+function debounce(func, wait, immediate) {
+    return function() {
+        const context = this;
+        const args = arguments;
+        let timeout;
+
+        const later = () => {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const slideImages = document.querySelectorAll('.slidePics');
+const checkSlideImages = () => {
+    slideImages.forEach(sliderImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
+};
+
+window.addEventListener('scroll', debounce(checkSlideImages, 100));
