b6b75ec54b78bb7075c5a301ad63b1dbf5d25925
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-12 16:05:58 -0700

    added a picture for header background, fixed most of the alignment in jumbotron and the contact card.

--- src/main.js
+++ src/main.js
@@ -1,39 +1,41 @@
 import './index.css'
 import 'flowbite'
 
-function debounce(func, wait, immediate) {
-    return function() {
-        const context = this;
-        const args = arguments;
-        let timeout;
-
-        const later = () => {
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
             timeout = null;
             if (!immediate) func.apply(context, args);
         };
-
-        const callNow = immediate && !timeout;
+        var callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
-
         if (callNow) func.apply(context, args);
     };
 }
 
-const slideImages = document.querySelectorAll('.slidePics');
-const checkSlideImages = () => {
-    slideImages.forEach(sliderImage => {
+const sliderImages = document.querySelectorAll('.slide-in');
+
+
+function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+        // half way through the image
         const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        //  bottom of the image
         const imageBottom = sliderImage.offsetTop + sliderImage.height;
         const isHalfShown = slideInAt > sliderImage.offsetTop;
         const isNotScrolledPast = window.scrollY < imageBottom;
-
         if (isHalfShown && isNotScrolledPast) {
             sliderImage.classList.add('active');
         } else {
             sliderImage.classList.remove('active');
         }
-    });
-};
 
-window.addEventListener('scroll', debounce(checkSlideImages, 100));
+
+    });
+
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
